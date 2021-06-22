b3c9ef18db520388d887f0d7759956cdfb44d663
Ian <52354240+ianmaciel7@users.noreply.github.com>
2021-06-22 17:34:49 -0300

    Adicionando OperationsController

---
+++ EStore.Api/Controllers/V1/OperationsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStore.Api.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_configuration;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
